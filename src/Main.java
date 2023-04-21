20df994c78e461d81ce1ed51a6598ab61f71b247
aikezhanakhmatova <alinaakhmatova77@gmail.com>
2023-04-21 18:17:44 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,724 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+
+       /* . Сиздер консольдон уч орундуу сан бересиздер, ошол санды
+        сиздер наобороттун чыгарып жана ортосунан болуп салышынздар
+                керек
+        Мисалы:
+
+        Input:  457
+        Output: 7 5 4
+        */
+//        System.out.print("Write a three bigt number:");
+//        int num4 = scan.nextInt();
+//
+//        int nam2 = num4 % 10;
+//        int nam3 = (num4 / 10) % 10;
+//        int nam5 = num4 / 100;
+//        System.out.println
+//        (nam2  + " " +nam3 + " " +nam5);
+
+
+
+       /* Сиздер консольдон 2 орундуу сан киргизишиниздер керек, ошол
+        санды сиздер математикалык турдоо консольго чыгарышыныздар
+        керек
+        Мисалы:
+
+        Input: 34
+        Output: 30 + 4  */
+
+//        int nam = scan.nextInt();
+//        int num  = nam % 10;
+//        System.out.print( num %100);
+
+
+       /* 3. Сиздер консольдон 2 орундуу сан киргизишиниздер керек, ошол
+        санды сиздер математикалык турдоо консольго чыгарышыныздар
+        керек
+        Мисалы:
+
+        Input: 34
+        Output: 30 + 4  */
+
+
+//        System.out.print("\nWrite a two bigt number:");
+//        System.out.print("\nInput:");
+//        byte s = scan.nextByte();
+//
+//        int  sifra = s / 10;
+//        int sifra2 = s % 10;
+//
+//        System.out.print("Output:" + sifra * 10 + "+" + sifra2);
+//
+//        System.out.print("\nInput:");
+//        byte t = scan.nextByte();
+//
+//        int  sifra3 = t / 10;
+//        int sifra4 = t % 10;
+//
+//        System.out.print("Output:" + sifra3 * 10 + "+" + sifra4);
+
+
+       /* 4. Сиздер консольдон 3 орундуу сан киргизишиниздер керек, ошол
+        санды сиздер математикалык турдоо консольго чыгарышыныздар
+        керек
+        Мисалы:
+        Input: 364
+        Output: 300 + 60 + 4  */
+
+//
+//        System.out.print("\nWrite a three bigt number:");
+//        System.out.print("'\nInput:");
+//        short v = scan.nextShort();
+//
+//        int b = v / 100;
+//        int n = (v % 100 ) / 10;
+//        int d = v % 10;
+//        System.out.print("\nOutput:" + b * 100 + "+" + n * 10 + "+" + d);
+
+
+        //  5 HOMEWORD  //
+
+       /* utput: 300 + 60 + 4
+        5. Напишите программу, который принимает с клавиатуры целое
+        положительное двузначное число. Затем следует построить новое
+        значение, составленное из цифр числа, введенного с клавиатуры,
+        но в обратном порядке. После этого новое значение следует
+        увеличить на 8, и окончательный результат вывести на экран.
+                Мисалы:
+
+        Input: 37
+        Output: 81 */
+
+
+//        int sifra = scan.nextInt();
+//
+//
+//        int nam7 = sifra % 10;
+//        int num8 = sifra / 10;
+//        int num1Nam2 = nam7 * 10 + num8;
+//
+//
+//        int result = num1Nam2 + 8;
+//
+//        System.out.println( result);
+//
+
+
+        // 6 HOMEWORD //
+
+
+      /*  Напишите программу, который принимает с клавиатуры целое
+        положительное трехзначное число. Затем следует построить новое
+        значение, составленное
+        из цифр числа, введенного с клавиатуры, но в обратном
+        порядке. После этого новое значение следует уменьшить на 20, и
+        окончательныйрезультат вывести на экран.
+
+        Мисалы:
+
+        Input: 327
+        Output: 703   */
+//
+//        System.out.print("Input:");
+//        int namber = scan.nextInt();
+//
+//        int sum1 = namber % 10;
+//        int sum2 = (namber / 10) % 10;
+//        int sum3 = namber / 100;
+//
+//         int newResult = sum1 * 100 + sum2 * 10 + sum3;
+//         int summa = newResult - 20;
+//        System.out.print("GeneralResult:" + summa);
+//
+//
+//        // 7 HOMEWORD  //
+
+
+
+      /*  Input: 359
+        Output: 3 + 5 + 9 = 17
+
+        Input: 409
+        Output: 4 + 0 + 9 = 13
+
+        8. Напишите программу, который принимает с клавиатуры целое */
+//
+//        System.out.print("\n Write number:");
+//        int number = scan.nextInt();
+//
+//        int san = number % 10;
+//        int san1 = (number / 10) % 10;
+//        int san2= number /100;
+//
+//        int result1 = san+san1+san2;
+//        System.out.print("Result: " +  san2 + "+" + san1 + "+" + san + "=" + result1);
+//
+//
+//        System.out.print("Input:");
+//        int para = scan.nextByte();
+//
+//        int value = para % 10;
+//        int value1 =(para / 10) % 10;
+//        int value2 = para % 100;
+//
+//        int value4 = value+value1+value2;
+//        System.out.print(" Output:" +value + "+" + value1 + "+" + value2 + "=" + value4);
+//
+
+
+        //  9  NOMEWORD  //
+
+
+       /* 9. Напишите программу, который принимает с клавиатуры целое
+        число и выводит на экран одно из следующих сообщений:
+“Положительное”, или “Отрицательное”, или “Ноль”, - в зависимости
+        от значения числа.*/
+
+//        System.out.print("Enter the number:");
+//        int can = scan.nextInt();
+//
+//        if(can>=0){
+//            System.out.println("Pozitiv");
+//        } else if (can<=0 ) {
+//            System.out.println(" Negativ:");
+//
+//        }else {
+//            System.out.println("Nol");
+//        }
+
+        //  10 HOMEWORD  //
+      /*  10. Напишите программу, который принимает с клавиатуры целое
+        положительное число и, если оно как минимум трехзначное и
+        положительное, уменьшает его на 1.
+
+        Мисалы:
+
+        Input: 332
+        Output: 331  */
+//
+//        System.out.print(" write number: ");
+//        int san = scan.nextInt();
+//
+//        int san1 = san-1;
+//        System.out.print( "Output;" +san1);
+//
+
+
+        // 11 HOMEWORD  //
+     /*   11. Напишите программу, который принимает с клавиатуры два
+        целых числа и, если первое больше второго, выводит на экран их
+        сумму, если же наоборот - выводит на экран их произведение. В
+        случае же, если числа одинаковы, программа выводит на экран
+        сообщение Числа равны  */
+
+
+//        int summa = scan.nextInt();
+//        int summa1 = scan.nextInt();
+//        int summa3 = summa+summa1;
+//        if (summa>summa1){
+//            System.out.print("Result:"  +summa3);
+//        } else if (summa<=summa1 && summa == summa1) {
+//            System.out.print("Proizvedenie chislo: "  +  summa + "+" +summa1);
+//
+//        }else{
+//            System.out.print(" Chislo rabny:");
+//        }
+
+        //  12 HOMEWORD  //
+    /*    12. Трехзначное положительное число называется «четно-
+                красивым», если каждая из его цифр - четная. Напишите класс,
+                который принимает с клавиатуры трехзначное число и проверяет,
+                является ли оно «четнокрасивым». В соответствии с результатом
+        проверки надо вывести на экран соответствующее текстовое
+        сообщение. Обратите внимание: задание имеет решение с
+        использованием одного оператора if и без составного услови  */
+
+//        int ran = scan.nextInt();
+//        int counter1 = ran % 2;
+//        if (ran % 2 == 0) {
+//            System.out.println("Chetno krasivye " + counter1);
+//        }
+
+
+        //   TODO MASSIVE  //
+
+//        int [] array = new int[5];
+//        array[0] = [1];
+//        array[1] = [2];
+//        array[2] = [3];
+//        array [3] = [4];
+//        array [4] =[5];
+
+
+//        String[] counters = new String[5];
+//        counters[0] = "KG";
+//        counters[1] = "USA";
+//        int[] nambers = {1, 2, 3, 4, 5};
+//
+//
+//        System.out.println(nambers.length);
+//        System.out.println(nambers[0]);
+////
+//        for (int i = 0; i <= 10; i++) {
+//            System.out.println(nambers[i]);
+
+
+//
+//            String [] language = {"lava, piton, php,ruby,js"};
+//            for (int j = 0; j <language.length ; j++) {
+//                System.out.println(language[j]);
+//
+//
+//                int counter = 0;
+//                while(counter<language.length){
+//                    System.out.println(language[counter]);
+//
+//
+//                    System.out.println(Arrays.toString(language)); //  ar bir indekstke kayrylyp maanini chygaryp beret //
+//
+//
+
+
+        /// TODO MASSIVDI RANDOM SANDARY MENEN CHYGARUU  ///
+
+        //     Random random = new Random();
+//                    int number = 1;
+//                    int number1 = random.nextInt(1,5);
+//                    System.out.println(number);
+//                    System.out.println(number1);
+
+
+//        int[] arrays = {2, 4, 6, 8, 5, 3, 1};
+//        int[] arrays1 = new int[5];
+//        for (int i = 0; i < arrays.length; i++) {
+//            arrays1[i] = random.nextInt(1, 5);
+//            System.out.println(arrays1[i]);
+
+
+        //  TODO MASSIVDIN BAARDYK MAANILERIN CHYGARUUNUN 3 JOLU BAR  ///
+
+
+//        int [] array = {1,2,3,4,5,6,7,8};
+//        int a = 0;
+//        while(a< array.length){
+//            System.out.println(array[a]);
+//        }
+
+
+//        int [] massive = {2,5,8,9,3,2,6};
+//        for (int i = 0; i <massive.length ; i++) {
+//            System.out.println(massive[i]);
+//
+//        }
+//        int [] array1 = {4,3,7,0,9,5,1};
+//        System.out.println(Arrays.toString(array1));
+
+
+        // HOMEWORD MASSIVE  //
+
+//        int[] array = {2,5,7,4,2,9,8};
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println(array[i]);
+//
+//
+//
+//
+//        }
+
+
+        //  TODO MYNDAY SAN LOK DEP CHYKSYN  TAPSHYRMA  ///
+
+
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//
+//        int[] massive = {1, 5, 3, 9, 7, 10, 35, 67, 49, 8, 55, 70, 84, 95, 100};
+//
+//        if (a>0) {
+//            System.out.println(massive[4]);
+//        }else if(a==0){
+//            System.out.println("Мындай сан жок");
+//
+//        }
+
+
+        // HOMEWORD 19  ///
+
+      /*  Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int counter = 0;
+        do {
+            a = a / counter;
+            System.out.println("Consist");
+        }while(a>=0);{
+            System.out.println("Pozitiv");
+        }*/
+
+
+        //   HOMEWORD 13  ///
+      /*  13. Бир программа жазыныздар, ал программа консольдон 2
+        орундуу сан алыш керек, бир гана if менен текшериш керек сандын
+        1 оруну жана 2 орунунун кобойтуусу чонбу же сан озубу
+
+        Мисалы:
+
+        Input: 25
+        Output: 25>10 */
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Input: ");
+//        int san1 = sc.nextInt();
+//        int sam = 1;
+//
+//        System.out.println("Output:");
+
+
+        ///  HOMEWORD 14  ///
+     /*   14. Бир программа жазыныз, сиз клавиатурадан эки сан беришиниз
+        керек, биринчи сан окуучулардын саны, экинчи отургучтардын саны.
+        Сиздер текшеришиниздер керек окуучуларга орун жетеби же
+        жетпейби.
+
+                Мисалы
+
+        Input: 25 30
+        Output: “Отургучтардын саны окуучулардан коп экен”
+
+        жана “Отургучтар жетет экен” , “Отургучтар жетпей калат деген”
+        вариантта  */
+//        Scanner scav = new Scanner(System.in);
+//        System.out.print("Input: ");
+//        int san2 = scav.nextInt();
+//        if(san2>0 && san2<=10){
+//            System.out.print("Отургучтар жетет экен");
+//        }else if(san2==0 || san2>5){
+//            System.out.print("Отургучтардын саны окуучулардан коп экен.");
+//        }else {
+//            System.out.println("Отургучтар жетпей калат экен.");
+//        }
+
+
+//        //  HOMEWORD 15  //
+     /*   15. Бир программа жазыныз, клавиатурадан уч сан ала турган, анан
+        аларды if, else if, else жардамдары менен ушул сандарды чоною
+        тартибинде чыгарасыздар консольго.
+
+                Мисалы:
+
+        Input: 2 243 1
+        Output: 1,  2,  243
+
+        Input: 1 -5 92
+        Output: -5, 1,  92   */
+//        Scanner sc1 = new Scanner(System.in);
+//        System.out.print("Input: ");
+
+
+        ///  TODO TAPSHYRMA //
+//
+//        Random random = new Random();
+//        int min = 50;
+//        int max = -50;
+//        for (int i = 0; i < 20; i++) {
+//            int x = random.nextInt(-50,50);
+//            System.out.println(x);
+//            if(min>x){
+//                min=x;
+//            }
+//            if(max<x){
+//                max=x;
+//            }
+//        }
+//        System.out.println("----------------------");
+//        System.out.println(min);
+//        System.out.println(max);
+
+
+//        Random random = new Random();
+//        int[] array = {2, 6, 1, 5, 8, 10, 15, 9, 7, 0, 12, 31, 35, 51, 23};
+//        int chetny = 14;
+//        int s = random.nextInt(0, 9);
+//
+//
+//            System.out.println(Arrays.toString(array));
+//            System.out.println("*********");
+//            System.out.print("Even number: ");
+
+
+//
+//        int[] array1 = new int[50];
+//        for (int i = 1; i < 100; i++) {
+//            if (i % 2 == 1) {
+//                System.out.print( "Taknsan:" + i + " , ");
+//                int s = i;
+//
+//
+//                int[] array = new int[50];
+//                for (int j = 1; j < 100; j--) {
+//                    if (i % 2 == 1) {
+//                        System.out.print( "Tak san:" + i + " , ");
+//                        int x = j;
+//
+//                    }
+//
+//                }
+//
+
+
+
+
+
+
+
+
+
+       /* 25. Напишите класс, который читает с клавиатуры символ и
+        проверяет, является ли введенный символ буквой латинского
+        алфавита. По результатам проверки следует вывести на экран
+        соответствующее сообщение.*/
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("write alfavit: ");
+//        String name = sc.nextLine();
+//        char simvol = sc.next().charAt(0);
+//        System.out.println(name.charAt(0) == simvol);
+
+
+
+
+
+
+       /* 26. Напишите класс, который принимает с клавиатуры два
+        строковых значения, каждое из которых содержит имя человека, и
+        проверяет, являются ли эти люди тезками. По результатам
+        проверки следует вывести на экран соответствующее сообщение.
+
+        */
+//
+//
+//        Scanner scan1 = new Scanner(System.in);
+//        System.out.print("Write name people: ");
+//        String name2 = scan1.nextLine();
+//        String name4 = scan1.nextLine();
+//        String name5 = scan.nextLine();
+//        System.out.println(name4.equals(name5) + "oni tezki");
+
+
+
+
+
+
+
+       /* 27. Напишите класс, который генерирует и выводит на экран
+        случайное целое чисел из диапазона положительных двузначных
+                чисел*/
+
+//        Random random = new Random();
+//        int a = 0;
+//        int namber = random.nextInt(7, 30);
+//        if (namber%2==0){
+////            System.out.print( "   This is an even number:" +namber);
+//
+//
+
+
+        /// TODO DLY CHISEL  //
+
+
+//
+//        Scanner num = new Scanner (System.in);
+//        int min = Integer.MAX_VALUE;
+//
+//        while (true)
+//            if (num.hasNextInt())
+//        {int nums = num.nextInt();
+//            if (nums <= min)
+//            {
+//                min = nums;
+//            }
+//            else if (nums >= min)
+//            {  int nums1 = nums+min;
+//                System.out.println();
+//
+//            }
+//        }
+//        else
+//
+//        { System.out.println(min);
+//        }
+
+
+//        Scanner console = new Scanner(System.in);
+//        int min = Integer.MAX_VALUE;
+//        int max =Integer.MAX_VALUE;
+//        while (console.hasNextInt()) {
+//            int x = console.nextInt();
+//            if (x < min){
+//                min = x;
+//
+//            }
+//
+//
+//        }
+//        System.out.print(min);
+//        System.out.println(max);
+//        System.out.println("Eti chislo ravny");
+//
+
+
+
+
+
+    /*    Scanner  scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int counter = 0;
+        while (a != 0) {
+            if (a % 10 == 2) {
+                counter++;
+            }
+            a =a / 10;
+        }
+        System.out.println(a);*/
+
+
+
+
+
+
+//        int[] array = {3, 4, 5, 6, 4};
+//        int[] array2 = {4, 3, 5, 6,1};
+
+        //myMassive(array, array2);
+//
+//        System.out.println(myReturn(array, array2));
+
+//
+//        myVoidMethod();
+//    }
+//
+//    public static void myMassive(int[] array, int[] array2) {
+//
+//
+//        int sum = 0;
+//        for (int j = 0; j < array.length; j++) {
+//            sum = sum + array[j];
+//        }
+//        System.out.println(sum);
+//
+//
+//        int sum1 = 0;
+//        for (int i = 0; i < array2.length; i++) {
+//            sum1 = sum1 + array2[i];
+//
+//        }
+//        System.out.println(sum1);
+//
+//
+//        if (sum != sum1) {
+//            System.out.println("not equal");
+//        }
+
+
+
+
+
+
+
+    /// TODO PRACTICA  ARRAY [] [] [] []   ///
+
+    /* public static int myReturn(int[] array, int[] array2) {
+        int nam = 0;
+        for (int i = 0; i < array.length; i++) {
+            return nam = nam + array[i];
+        }
+        System.out.println(nam);
+
+        int num = 0;
+        for (int j = 0; j < array2.length; j++) {
+            return num = num + array2[j];}
+        System.out.println(num);
+        if (nam != num) {
+            System.out.println("Barabar");
+        } else {
+            System.out.println("Barabar emes");
+        }return 0;*/
+
+
+
+
+//
+//}
+//    public  static void  myVoidMethod(int b){
+//
+//
+//                if(b%2==0){
+//            System.out.println("Positiv number");
+//        }else if(b%2==1){
+//            System.out.println("Negativ number");
+//        }else{
+//                    System.out.println("Error");
+//                }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+//
+//            public static int myRetufnMethod(int[], int[])
+//                int [] massive = {5,6,7,3,2,1,9};
+//                int [] massive2 = {34,2,12,67,4,5,};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
